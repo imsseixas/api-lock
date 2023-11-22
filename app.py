37a3fc96e678f7226026c0139f0d521ba488050b
Italo Matheus Souza Seixas <115546560+imsseixas@users.noreply.github.com>
2023-11-22 19:12:20 -0300

    Update app.py agora vai

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
 from flask import Flask, jsonify
-import mysql.connector
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 # Configurações do banco de dados
-db = None
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lock_dart:Dart#Lock23@lock_dart.mysql.dbaas.com.br:3306/lock_dart'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 300  # Recicla conexões a cada 300 segundos
 
-try:
-    db = mysql.connector.connect(
-        host='lock_dart.mysql.dbaas.com.br',
-        port=3306,
-        user='lock_dart',
-        password='Dart#Lock23',
-        database='lock_dart'
-    )
-    print("Conexão ao banco de dados bem-sucedida!")
-except mysql.connector.Error as e:
-    print(f"Erro ao conectar ao banco de dados: {e}")
-    # Tratar o erro, como registrar ou notificar alguém responsável
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    cod_usuario = db.Column(db.Integer, primary_key=True)
+    bars_code = db.Column(db.String(255))  # Assumindo que 'bars_code' é uma string de até 255 caracteres
+    # Adicione outras colunas correspondentes à sua tabela
 
 @app.route('/dados', methods=['GET'])
 def obter_dados():
-    if db is None:
-        return jsonify({'error': 'Erro ao conectar ao banco de dados'})
-
     try:
-        cursor = db.cursor()
-        cursor.execute('SELECT * FROM usuarios')
-        dados = cursor.fetchall()
-        cursor.close()
+        usuarios = Usuario.query.all()
+        dados = [
+            {'cod_usuario': usuario.cod_usuario, 'bars_code': usuario.bars_code}
+            for usuario in usuarios
+        ]
         return jsonify(dados)
-    except mysql.connector.Error as e:
-        print(f"Erro ao executar a consulta SQL: {e}")
-        # Aqui você pode lidar com o erro de acordo com sua lógica de aplicação
-        return jsonify({'error': f'Erro ao obter dados do banco de dados: {str(e)}'})
+    except Exception as e:
+        print(f"Erro ao obter dados do banco de dados: {e}")
+        return jsonify({'error': 'Erro ao obter dados do banco de dados'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
